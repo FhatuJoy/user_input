4e4ca641c64a0ba703ff5ead834b5f5abbdea6b9
fhatu joy <fhatubagi@gmail.com>
2024-03-10 04:40:20 +0200

    initial commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Ask for user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you from? ")
+
+# Print out a personalized message
+print(f"Hello, {name}! You are {age} years old and from {location}. It's nice to meet you!")
